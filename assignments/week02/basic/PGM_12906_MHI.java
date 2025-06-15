dd6c836932708f58fb28899f544a547295af6664
moon <mhi0118@naver.com>
2025-06-15 10:39:32 +0900

    solve: [PGM] 12906 - 같은 숫자는 싫어 완료

--- assignments/week02/basic/PGM_12906_MHI.java
+++ assignments/week02/basic/PGM_12906_MHI.java
@@ -1,25 +1,23 @@
 import java.util.*;
-class Solution {
-    public String solution(String[] participant, String[] completion) {
 
-        HashMap<String, Integer> map = new HashMap();
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> st = new Stack<>();
 
-        for (String word : completion) {
-            if (map.containsKey(word))
-                map.put(word, map.get(word) + 1);
-            else
-                map.put(word, 1);
-        }
-
-        for (String word : participant) {
-            if (map.containsKey(word)) {
-                if (map.get(word) == 0) return word;
-                else map.put(word, map.get(word) - 1);
-            } else {
-                return word;
+        for (int num : arr) {
+            if (st.isEmpty() || st.peek() != num) {
+                st.push(num);
             }
         }
 
-        return "";
+        int[] answer = new int[st.size()];
+        int i = st.size() - 1;
+
+        while (!st.isEmpty()) {
+            answer[i] = st.pop();
+            i--;
+        }
+
+        return answer;
     }
 }
