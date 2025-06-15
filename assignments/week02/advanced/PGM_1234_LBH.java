acf8f7159860b192b8ec8fdf16ac4ae6e401f5b3
aram <miaou808@gmail.com>
2025-06-16 02:31:59 +0900

    [PGM] 42583 - 다리를 지나는 트럭, 42627 - 디스크 컨트롤러 풀이 완료

---
+++ assignments/week02/advanced/PGM_1234_LBH.java
@@ -0,0 +1,11 @@
+// 아래 주석 양식을 필히 작성하여 주세요.
+// PGM 1234 - 완전범죄
+// 유형: 이분 탐색
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/389480?language=python3
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
