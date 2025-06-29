af33d5f0798e0416f00ff6a0a24a3016f30faf0e
DONGJUN82 <bae9077@naverc.com>
2025-06-29 18:05:30 +0900

    solve: [PGM] 42884 - 단속카메라 풀이완료

--- assignments/weeks04/advanced/PGM_42884_SDJ.java
+++ assignments/weeks04/advanced/PGM_42884_SDJ.java
@@ -6,35 +6,34 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] routes) {
-		Arrays.sort(routes, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1]==o2[1]) {
-					return o1[0]-o2[0];
-				}
-				return o1[1]-o2[1]; 
+public int solution(int[][] routes) {
+	Arrays.sort(routes, new Comparator<int[]>() {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			if(o1[1]==o2[1]) {
+				return o1[0]-o2[0];
 			}
-		});   
-		
-		int count=0;
-		int now = 0;
-		for(int idx=0; idx<routes.length; idx++) {
-            
-            int start = routes[idx][0];
-            int end  = routes[idx][1];
-			if(count == 0) { //첫번째는 무조건 카메라 설치
-				now = end;
-				count++;
-			}
+			return o1[1]-o2[1]; 
+		}
+	});   
 
-			if(now >= start && now <= end) { //이전 구간 안에 있으면 카메라 미설치
-			} else {
-				now = end;
-				count++;
-			}
-            
-        }
-		return count;
-    }
+	int count=0;
+	int now = 0;
+	for(int idx=0; idx<routes.length; idx++) {
+
+		int start = routes[idx][0];
+		int end  = routes[idx][1];
+		if(count == 0) { //첫번째는 무조건 카메라 설치
+			now = end;
+			count++;
+		}
+
+		if(now >= start && now <= end) { //이전 구간 안에 있으면 카메라 미설치
+		} else {
+			now = end;
+			count++;
+		}
+	}
+	return count;
+	}
 }
