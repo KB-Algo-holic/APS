934f1922079373ca7ca33ab4b053abcb9aba6e10
JunHong-Choi <chltnsqhr@gmail.com>
2025-07-09 17:01:30 +0900

    test: [PGM] 1844 - 테스트용 커밋.

---
+++ assignments/week06/basic/PGM_1844_CJH.java
@@ -0,0 +1,10 @@
+// PGM 1844 - 게임맵 최단거리
+// 유형 : BFS
+// 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+class Solution {
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
+}
