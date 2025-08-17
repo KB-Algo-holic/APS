04c82f62f636f18b8f70962aace10b78d4d8b3bc
mrcodemania <milk1543@naver.com>
2025-08-17 16:04:06 +0900

    fix: 이전 소스 반영으로 인한 수정

--- assignments/week11/LBH/PGM_340210_LBH.java
+++ assignments/week11/LBH/PGM_340210_LBH.java
@@ -8,13 +8,13 @@ class Solution {
 
     List<String[]> correct = new ArrayList<String[]>();
     List<String[]> erase = new ArrayList<String[]>();
-    boolean[] bases = new int[10];
+    boolean[] bases = new boolean[10];
     String[] answer;
 
-    int convertBase(String num, int base){
+    int convertDecimal(String num, int base){
         int sum = 0;
-        for(int i = 0; i < num.length; i++) {
-            sum += (int)num.charAt(i) * Math.pow(base, num.length - (i + 1));
+        for(int i = 0; i < num.length(); i++) {
+            sum += (int)(num.charAt(i) - '0') * Math.pow(base, num.length() - (i + 1));
         }
         return sum;
     }
@@ -23,14 +23,48 @@ class Solution {
         for(int j = 2; j <= 9; j++) {
             boolean flag = true;
 
+
+            for(int i = 0; i < erase.size(); i++) {
+                for (int t = 0; t <= 2; t++) {
+                    if (t != 1) {
+                        if (erase.get(i)[t].charAt(0) - '0' >= j) {
+                            if (erase.get(i)[t].length() > 1) {
+                                if (erase.get(i)[t].charAt(1) - '0' >= j) {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
             for(int i = 0; i < correct.size(); i++) {
+
+                for(int t = 0; t <= 3; t++) {
+                    if(t != 1) {
+                        if (correct.get(i)[t].charAt(0) - '0' >= j) {
+                            if (correct.get(i)[t].length() > 1) {
+                                if (correct.get(i)[t].charAt(1) - '0' >= j) {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+
                 if("+".equals(correct.get(i)[1])){
-                    if(convertDecimal(correct.get(i)[0]) + convertDecimal(correct.get(i)[2]) != convertDecimal(correct.get(i)[3])){
+                    if(convertDecimal(correct.get(i)[0], j) + convertDecimal(correct.get(i)[2], j) != convertDecimal(correct.get(i)[3], j)){
                         flag = false;
                         break;
                     }
                 } else if("-".equals(correct.get(i)[1])){
-                    if(convertDecimal(correct.get(i)[0]) - convertDecimal(correct.get(i)[2]) != convertDecimal(correct.get(i)[3])){
+                    if(convertDecimal(correct.get(i)[0], j) - convertDecimal(correct.get(i)[2], j) != convertDecimal(correct.get(i)[3], j)){
                         flag = false;
                         break;
                     }
@@ -54,8 +88,19 @@ class Solution {
                 if(bases[j]) {
                     if("+".equals(erase.get(i)[1])){
                         if(first) {
-                            result = convertDecimal(erase.get(i)[0]) + convertDecimal(erase.get(i)[2]);
-                        } else if(result != (convertDecimal(erase.get(i)[0]) + convertDecimal(correct.get(i)[2]))){
+                            result = convertDecimal(erase.get(i)[0], j) + convertDecimal(erase.get(i)[2], j);
+                            result = Integer.parseInt(Integer.toString(result, j));
+                            first = false;
+                        } else if(result != Integer.parseInt(Integer.toString(convertDecimal(erase.get(i)[0], j) + convertDecimal(erase.get(i)[2], j), j))){
+                            flag = false;
+                            break;
+                        }
+                    } else {
+                        if(first) {
+                            result = convertDecimal(erase.get(i)[0], j) - convertDecimal(erase.get(i)[2], j);
+                            result = Integer.parseInt(Integer.toString(result, j));
+                            first = false;
+                        } else if(result != Integer.parseInt(Integer.toString(convertDecimal(erase.get(i)[0], j) - convertDecimal(erase.get(i)[2], j), j))){
                             flag = false;
                             break;
                         }
@@ -65,16 +110,9 @@ class Solution {
 
             // 진법화 시켜야할듯?
             if(flag) {
-                int right = 0;
-                for(int j = 2; j <= 9; j++) {
-                    if(bases[j]) {
-                        right = convertBase("" + result, j);
-                        break;
-                    }
-                }
-                answer[i] = {erase.get(i)[0] + " " + erase.get(i)[1] + " " + erase.get(i)[2] + " = " + right};
+                answer[i] = erase.get(i)[0] + " " + erase.get(i)[1] + " " + erase.get(i)[2] + " = " + result;
             } else {
-                answer[i] = {erase.get(i)[0] + " " + erase.get(i)[1] + " " + erase.get(i)[2] + " = " + "?"};
+                answer[i] = erase.get(i)[0] + " " + erase.get(i)[1] + " " + erase.get(i)[2] + " = " + "?";
             }
         }
     }
@@ -86,13 +124,13 @@ class Solution {
         String right;
 
         for(String s : expressions){
-            String[] left = s.split(" = ")[0].split(" ");
-            String right = s.split(" = ")[1];
+            left = s.split(" = ")[0].split(" ");
+            right = s.split(" = ")[1];
 
             if(!"X".equals(right)){
-                correct.add(new String{left[0], left[1], left[2], right});
+                correct.add(new String[]{left[0], left[1], left[2], right});
             } else {
-                erase.add(new String{left[0], left[1], left[2], right});
+                erase.add(new String[]{left[0], left[1], left[2], right});
             }
         }
 
@@ -109,7 +147,8 @@ class Solution {
         return answer;
     }
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        Solution S = new Solution();
+        System.out.println(S.solution(new String[]{"1 + 1 = 2", "1 + 3 = 4", "1 + 5 = X", "1 + 2 = X"}));
     }
 }
