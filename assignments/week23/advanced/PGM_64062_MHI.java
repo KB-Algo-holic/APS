e9a1a5e8204c6c773ab51cfca59e675baf6cd198
moon <mhi0118@naver.com>
2025-11-08 22:12:26 +0900

    solve: [PGM] 64042 - 징검다리 건너기 풀이

--- assignments/week23/advanced/PGM_64062_MHI.java
+++ assignments/week23/advanced/PGM_64062_MHI.java
@@ -1,31 +1,26 @@
-import java.util.ArrayList;
-
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (check(s, i)) answer++;
-        }
-        return answer;
-    }
 
-    public boolean check(String s, int i) {
-        String temp = s.substring(i) + s.substring(0, i);
-        ArrayList<Character> st = new ArrayList<>();
+    public int solution(int[] stones, int k) {
 
-        for (char c : temp.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                st.add(c);
-            } else if (st.isEmpty()) {
-                return false;
-            } else if ((st.get(st.size() - 1) == '(' && c == ')')
-                    || (st.get(st.size() - 1) == '[' && c == ']')
-                    || (st.get(st.size() - 1) == '{' && c == '}')) {
-                st.remove(st.size() - 1);
-            } else {
-                return false;
+        int l=1,r=200000000;
+
+        while(l<r) {
+
+            int mid=(l+r)/2;
+            int count=0;
+
+            for(int i=0; i<stones.length; i++) {
+                if(stones[i]<=mid) {
+                    count++;
+                    if(count==k) {
+                        r=mid;
+                        break;
+                    }
+                }
+                else count=0;
             }
+            if(r!=mid) l=mid+1;
         }
-        return st.isEmpty();
+        return l;
     }
 }
