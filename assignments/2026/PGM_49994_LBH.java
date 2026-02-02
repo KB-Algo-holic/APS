7e2c5d8787ecf2440a8e6786a2bfc809f062f08b
mrcodemania <milk1543@naver.com>
2026-02-02 20:55:53 +0900

    chore: create 2026 directory

---
+++ assignments/2026/PGM_49994_LBH.java
@@ -0,0 +1,11 @@
+// PGM 49994 - 방문 길이
+// 구현
+// https://school.programmers.co.kr/learn/courses/30/lessons/49994
+
+class Solution {
+    public int solution(String dirs) {
+        int answer = 0;
+
+        return answer;
+    }
+}
