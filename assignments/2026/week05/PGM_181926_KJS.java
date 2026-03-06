2dcec627171b56ab06aa66e08446108e18351e27
formalKim <zxclkj2@gmail.com>
2026-03-06 22:08:29 +0900

    solve: [PGM] 81926 - 수조작하기 풀이완

--- assignments/2026/week05/PGM_181926_KJS.java
+++ assignments/2026/week05/PGM_181926_KJS.java
@@ -1,1 +1,26 @@
+// PGM 181926 - 수조작하기
+// 구현
+// https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=java
 
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        for (int i = 0; i < control.length(); i++){
+            switch(control.charAt(i)){
+                case 'w' : answer += 1;
+                break;
+                
+                case 's' : answer -= 1;
+                break;
+                
+                case 'd' : answer += 10;
+                break;
+                    
+                case 'a' : answer -= 10;
+                break;
+            }
+        }
+        return answer;
+    }
+}
