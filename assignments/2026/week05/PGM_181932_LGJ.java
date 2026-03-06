4cd7532720787409e0d58d6318f5897da4ae69d0
Gyujeong-Lee <rbwjd0101@naver.com>
2026-03-06 21:50:51 +0900

    solve: [PGM] 181932 - 코드 처리하기 풀이 완료

---
+++ assignments/2026/week05/PGM_181932_LGJ.java
@@ -0,0 +1,21 @@
+// PGM - 181932
+// 구현
+// https://school.programmers.co.kr/learn/courses/30/lessons/181932
+
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+
+        for (int i=0; i<code.length(); i++){
+            if (mode == 0) {
+                if (code.charAt(i) == '1') mode = 1;
+                else if (i % 2 == 0) answer += code.charAt(i);
+            } else if (mode == 1) {
+                if (code.charAt(i) == '1') mode = 0;
+                else if (i % 2 == 1) answer += code.charAt(i);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer;
+    }
+}
