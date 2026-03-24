fc26dfea8bda642cfbd80d22e8dd7a4c8d2e5ac5
Chae-Eun Jeong <68576770+Chae-EunJeong@users.noreply.github.com>
2026-03-24 22:25:48 +0900

    solve: [PGM] 12915 - 문자열 내 마음대로 정렬하기 풀이 완료

---
+++ assignments/2026/week08/PGM_12915_JCE.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        Arrays.sort(strings, (a, b) -> {
+            
+            // n번째 글자 같으면 사전순 정렬 
+            if (a.charAt(n) == b.charAt(n)) {
+                return a.compareTo(b);
+            }
+            // n번째 글자 기준 오름차순 정렬
+            return a.charAt(n) - b.charAt(n);
+        });
+        return strings;
+    }
+}
