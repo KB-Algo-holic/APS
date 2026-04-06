298f011c373fa87a737dff57c039f70944d461de
Gyujeong-Lee <rbwjd0101@naver.com>
2026-04-06 22:41:09 +0900

    docs: update stack.md in study

--- assignments/2026/week09/PGM_12909_LGJ.java
+++ assignments/2026/week09/PGM_12909_LGJ.java
@@ -7,14 +7,14 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
 
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for (int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(') {
-                stack.push(String.valueOf(c));
+                stack.push(c);
             } else {
                 if (stack.isEmpty()) return false;
-                else if (stack.peek().equals(String.valueOf(')'))) return false;
+                else if (stack.peek() == ')') return false;
                 else stack.pop();
             }
         }
