dbf7c16ba6516af8561f11d76af321d2e7531522
unknown <hara0822@naver.com>
2026-04-12 21:24:13 +0900

    solve: [PGM] 42576 - 완주하지 못한 선수

---
+++ assignments/2026/week10/PGM_42576_SEJ.java
@@ -0,0 +1,34 @@
+// PGM - 42576 완주하지 못한 선수
+// 해시
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p,0)+1);   
+        }
+        
+        for(String c : completion){
+            int cnt = map.get(c)-1;
+            if(cnt>0){
+                map.put(c, cnt);
+                continue;
+            }
+            map.remove(c);
+        }
+        
+        String result = "";
+        for(String p : participant){
+            if(!map.containsKey(p)) continue;
+            result = p;
+        }
+        
+        return result;
+    }
+}
