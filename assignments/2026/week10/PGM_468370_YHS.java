58eef2ce11d3d4b9a560235805b0639ea6e00fb6
Hyunsoo <laugh4mile@naver.com>
2026-04-17 19:49:20 +0900

    solve: [PGM] 172928 - 공원산책 풀이완료

---
+++ assignments/2026/week10/PGM_468370_YHS.java
@@ -0,0 +1,71 @@
+// PGM - 468370 중요한 단어를 스포 방지
+// 해시
+// https://school.programmers.co.kr/learn/courses/30/lessons/468370
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PGM_468370_YHS {
+    public int solution(String message, int[][] spoiler_ranges) {
+        int answer = 0;
+        // Map<String, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        // 스포방지 인덱스 저장
+        for(int i=0; i<spoiler_ranges.length; i++){
+            for(int j=spoiler_ranges[i][0]; j<=spoiler_ranges[i][1]; j++){
+                set.add(j);
+            }
+        }
+
+        Map<String, Integer> words = new HashMap<>();
+        Map<String, Integer> hiddenWords = new HashMap<>();
+
+        // for문 돌면서 스포방지 단어인지 아닌지 판별
+        int s = 0;
+        boolean flag = false;
+        for(int i=0; i<message.length(); i++){
+            char c = message.charAt(i);
+            if(c !=' ' && set.contains(i)){
+                flag = true;
+            }
+
+            if(c == ' '){
+                String word = message.substring(s,i);
+
+                if(flag){
+                    hiddenWords.put(word, 1);
+                }else{
+                    words.put(word, 1);
+                }
+
+                s = i+1;
+                flag = false;
+            }
+        }
+        for(int i=s; i<message.length(); i++){
+            char c = message.charAt(i);
+
+            if(set.contains(i)){
+                flag = true;
+            }
+        }
+
+        String word = message.substring(s);
+
+        if(flag){
+            hiddenWords.put(word, 1);
+        }else{
+            words.put(word, 1);
+        }
+
+        for(String key : hiddenWords.keySet()){
+            if(words.get(key) == null){
+                words.put(key, 1);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
---
+++ assignments/2026/week11/PGM_92334_YHS.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+// PGM - 92334 L1_신고결과받기
+// 해시
+// https://school.programmers.co.kr/learn/courses/30/lessons/92334
+public class PGM_92334_YHS {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        Set<String> set = new HashSet<>();
+
+        // 중복신고 제거
+        for(int i=0; i<report.length; i++){
+            set.add(report[i]);
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : set){
+            String from = s.split(" ")[0];
+            String to   = s.split(" ")[1];
+
+            if(map.get(to) == null){
+                map.put(to, 1);
+            }else{
+                map.put(to, map.get(to)+1);
+            }
+        }
+
+        Map<String, Integer> result = new HashMap<>();
+
+        for(String s : set){
+            String from = s.split(" ")[0];
+            String to   = s.split(" ")[1];
+
+            if(map.get(to) != null && map.get(to) >= k){
+                if(result.get(from) == null){
+                    result.put(from, 1);
+                }else{
+                    result.put(from, result.get(from)+1);
+                }
+            }
+        }
+
+        for(int i=0; i<id_list.length; i++){
+            if(result.get(id_list[i]) == null){
+                answer[i] = 0;
+            }else{
+                answer[i] = result.get(id_list[i]);
+            }
+        }
+
+        return answer;
+    }
+}
