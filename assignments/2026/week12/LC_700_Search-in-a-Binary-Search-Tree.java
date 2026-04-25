830ed8ff4b9c070962ac487b4b534eaef4ea16e9
Chae-Eun Jeong <68576770+Chae-EunJeong@users.noreply.github.com>
2026-04-25 11:34:21 +0900

    solve: [LC] 700 - Search in a Binary Search Tree 풀이 완료

---
+++ assignments/2026/week12/LC_700_Search-in-a-Binary-Search-Tree.java
@@ -0,0 +1,39 @@
+/*
+문제 : 700. Search in a Binary Search Tree
+링크 : https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if(root == null) {
+            return null;
+        }
+        
+        if(root.val == val) {
+            return root;
+        }
+        
+        if(root.val > val) {
+            return searchBST(root.left, val);
+        }
+        
+        else {
+            return searchBST(root.right, val);
+        }
+    }
+}
