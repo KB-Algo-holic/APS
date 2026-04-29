8c55d60387a9fa99e2e516cd44bca5a0e995f936
Chae-Eun Jeong <68576770+Chae-EunJeong@users.noreply.github.com>
2026-04-29 21:59:53 +0900

    solve: [PGM] 138477 - 명예의 전당 (1) 풀이 완료

---
+++ assignments/2026/week13/PGM_138477_JCE.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(score[i]);
+                answer[i] = pq.peek();
+            } else {
+                pq.offer(score[i]);
+                pq.poll();
+                answer[i] = pq.peek();
+            }
+        }
+        return answer;
+    }
+}
