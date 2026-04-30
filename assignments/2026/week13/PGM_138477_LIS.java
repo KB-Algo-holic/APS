a404e4912ee1c2cded17ed0eec8e5409ccca9224
ikseunglee-note <ikseunglee@gmail.com>
2026-04-30 11:51:32 +0900

    solve:[PGM] 138477 - 명예의전당(1)

--- assignments/2026/week13/PGM_138477_LIS.java
+++ assignments/2026/week13/PGM_138477_LIS.java
@@ -1,26 +1,20 @@
-// PGM 138477 - 명예의전당
+// PGM 138477 - 명예의전당(1)
 // 구현
 // https://school.programmers.co.kr/learn/courses/30/lessons/138477
 import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
+        
         int[] answer = new int[score.length];
         
-        List<Integer> aList = new ArrayList(); 
-
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
         for(int i=0; i < score.length; i++) {
-            if(i < k) {
-                aList.add(score[i]);
-                
-            } else {              
-                if(score[i] >= aList.get(0)){
-                    aList.remove(0);
-                    aList.add(score[i]);
-                }
-            }
-            Collections.sort(aList);
-            answer[i] = aList.get(0);
+            pq.add(score[i]);
+            if(i > (k-1)) pq.poll();                     
+        
+            answer[i] = pq.peek();
         }
         return answer;
     }
