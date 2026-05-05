0196ba23aa0e0817cd35acca44b5211a123f2f81
함성호 <hamseongho@hamseonghoui-MacBookAir.local>
2026-05-05 12:05:05 +0900

    solve: [LC] 100 Same Tree - 기본 문제 해결

---
+++ assignments/2026/week14/LC_100_HSH.java
@@ -0,0 +1,45 @@
+package week14;
+
+
+public class LC_100_HSH {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        LC_100_HSH solution = new LC_100_HSH();
+        TreeNode p = new TreeNode(1);
+        TreeNode q = new TreeNode(1);
+        boolean result = solution.isSameTree(p, q);
+        System.out.println(result);
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
