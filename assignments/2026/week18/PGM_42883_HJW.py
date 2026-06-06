6c717fd789684e2be0b1fa8e24581dc23e46c4d8
WonHwang <ediblepotato@gmail.com>
2026-06-06 17:27:00 +0900

    solve: [PGM] 42883 - 큰 수 만들기 풀이 완료

---
+++ assignments/2026/week18/PGM_42883_HJW.py
@@ -0,0 +1,23 @@
+# PGM 42883 - 큰 수 만들기
+# 유형: 그리디
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+def solution(number, k):
+    number = list(number)
+    stack = []
+    for num in number:
+        if not stack:
+            stack.append(num)
+            continue
+        
+        while stack and k and stack[-1] < num:
+            stack.pop()
+            k -= 1
+        
+        stack.append(num)
+    
+    while k:
+        stack.pop()
+        k -= 1
+        
+    return ''.join(stack)
