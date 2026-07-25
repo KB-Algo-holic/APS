9ce410c9c0ffdf6de4d2c3ba2f98376a70be2a28
Hyunsoo <laugh4mile@naver.com>
2026-07-25 13:55:24 +0900

    solve: [PGM] 49993 - 스킬트리 풀이완료

---
+++ assignments/2026/week25/PGM_49993_YHS.java
@@ -0,0 +1,40 @@
+// PGM - 49993 스킬트리
+// 그래프
+// https://school.programmers.co.kr/learn/courses/30/lessons/49993
+import java.util.HashMap;
+import java.util.Map;
+
+public class PGM_49993_YHS {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i=0; i<skill.length(); i++){
+            char c = skill.charAt(i);
+            map.put(c, i+1);
+        }
+
+        outer: for(int i=0; i<skill_trees.length; i++){
+            int cnt = 1;
+            for(int j=0; j<skill_trees[i].length(); j++){
+                char c = skill_trees[i].charAt(j);
+                // System.out.println(c);
+
+                if(map.get(c) != null){
+                    if(map.get(c) == cnt){
+                        cnt++;
+                    }else{
+                        // System.out.println();
+                        continue outer;
+                    }
+                }
+            }
+            answer++;
+            System.out.println();
+        }
+
+
+        return answer;
+    }
+}
