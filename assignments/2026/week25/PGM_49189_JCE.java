d44d268bb8ce6619e167075500b34462b8fcc8f6
Chae-Eun Jeong <68576770+Chae-EunJeong@users.noreply.github.com>
2026-08-10 22:10:56 +0900

    solve: [PGM] 49189 - 가장 먼 노드 풀이 완료

--- assignments/2026/week25/PGM_49189_JCE.java
+++ assignments/2026/week25/PGM_49189_JCE.java
@@ -1,7 +1,7 @@
 /* 
  * PGM 49189 - 가장 먼 노드
  * https://school.programmers.co.kr/learn/courses/30/lessons/49189
-
+ */
 import java.util.*;
 class Solution {
     public static int solution(int n, int[][] edge) {
