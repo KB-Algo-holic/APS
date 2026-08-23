d754e08b2068d2e1a20139f4748d24315563f44f
HighMoon0118 <mhi0118@naver.com>
2026-08-23 23:00:25 +0900

    solve: [PGM] 42898 - 등굣길 풀이 완료

--- assignments/2026/week29/PGM_42898_MHI.java
+++ assignments/2026/week29/PGM_42898_MHI.java
@@ -1,21 +1,39 @@
-// PGM 161989 - 덧칠하기
-// 그리디
-// https://school.programmers.co.kr/learn/courses/30/lessons/161989
+// PGM 42898 - 등굣길
+// 동적계획법
+// https://school.programmers.co.kr/learn/courses/30/lessons/42898
 
 import java.util.*;
-
 class Solution {
-    public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        int painted = 0;
 
-        for (int i = 0; i < section.length; i++) {
-            if (section[i] > painted) {
-                answer++;
-                painted = section[i] + m - 1;
+    static int[][] dp, map;
+    static int[] dr = {1,0}, dc = {0, 1};
+    public int solution(int m, int n, int[][] puddles) {
+        dp = new int[m][n];
+        for(int i=0; i<m; i++) Arrays.fill(dp[i], -1);
+        
+        map = new int[m][n];
+        for(int i=0; i<puddles.length; i++){
+            map[puddles[i][0] - 1][puddles[i][1] - 1] = 1;
+        }
+        
+        return makeAnswer(0,0, m, n);
+    }
+    public int makeAnswer(int m, int n, int M, int N) {
+
+        if(m == M-1 && n == N-1) return 1;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int result = 0;
+        for(int i=0; i<2; i++) {
+            int nextR = m+dr[i];
+            int nextC = n+dc[i];
+            if(nextR < M && nextC < N && map[nextR][nextC] == 0){
+                result += makeAnswer(nextR, nextC, M, N);
+                result %= 1000000007;
             }
         }
-
-        return answer;
+        
+        return dp[m][n] = result;
     }
-}}
+}
