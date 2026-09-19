3c2181f5b83fce43d3f2a028ce905e18c4d1f794
Gyujeong-Lee <rbwjd0101@naver.com>
2026-09-19 14:33:06 +0900

    solve: [LC] 526 Beautiful Arrangement 풀이완료

---
+++ assignments/2026/week33/LC_526_LGJ.java
@@ -0,0 +1,45 @@
+// LC 526 Beautiful Arrangement
+// 완전탐색(순열)
+// https://leetcode.com/problems/beautiful-arrangement/description/?utm_source=chatgpt.com
+
+class Solution {
+
+    boolean[] visited;
+    int answer = 0;
+
+    public int countArrangement(int n) {
+
+        visited = new boolean[n + 1];
+
+        perm(n, 0);
+
+        return answer;
+    }
+
+    private void perm(int n, int idx) {
+
+        if (idx == n) {
+            answer++;
+            return;
+        }
+
+        int position = idx + 1;
+
+        for (int num = 1; num <= n; num++) {
+
+            if (visited[num]) continue;
+
+            // 이 위치에 num을 놓을 수 있는가?
+            if (num % position != 0 &&
+                    position % num != 0) {
+                continue;
+            }
+
+            visited[num] = true;
+
+            perm(n, idx + 1);
+
+            visited[num] = false;
+        }
+    }
+}
